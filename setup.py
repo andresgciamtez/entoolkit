eed20c322e61bd5462946ffd968fd939c7b179d4
andresgciamtez <49898742+andresgciamtez@users.noreply.github.com>
2019-07-24 11:25:40 +0200

    Add files via upload
    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from distutils.core import Extension
+
+DISTNAME = 'epanettk'
+VERSION = '0.1.0'
+PACKAGES = find_packages()
+LONG_DESCRIPTION = open('README.md', encoding="utf8").read()
+
+setup(name='epanettk',
+      version=VERSION,
+      packages=PACKAGES,
+      description='Epanet toolkit wrapper',
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type='text/markdown',
+      author='Andrés García Martínez',
+      author_email='ppnoptimizer@gmail.com',
+      license='GPL 2.0',
+      url='https://github.com/andresgciamtez/epanettk',
+)
