47f29d1f164655ff372ef45b88d464a67bfd16e3
andresgciamtez <49898742+andresgciamtez@users.noreply.github.com>
2019-07-26 11:04:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-VERSION = '0.1.0'
+VERSION = '0.1.2'
 PACKAGES = find_packages()
 LONG_DESCRIPTION = open('README.md', encoding="utf8").read()
 
