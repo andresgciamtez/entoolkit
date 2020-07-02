3834b26becbca5ce1f7d389a8744b17263ccc326
andresgciamtez <49898742+andresgciamtez@users.noreply.github.com>
2020-07-02 22:50:30 +0200

    load lib bug

--- README.md
+++ README.md
@@ -1,21 +1,8 @@
-# Epanet Toolkit Wrapper for Python
+# Python extension for the EPANET Programmers Toolkit
 https://www.epa.gov/water-research/epanet
 
 # Source
-
 Adapted from https://github.com/USEPA/WNTR and others
-
-## Install
-
-```console
-> pip install entoolkit
-```
-## Load module
-
-```python
-from entoolkit import toolkit as et
-```
 
 ## Help
 https://www.epa.gov/water-research/epanet
-
---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='entoolkit',
+    version='0.1.0',
+    description='Python extension for the EPANET Programmers Toolkit',
+    long_description=open('./README.md', encoding="utf8").read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/andresgciamtez/entoolkit',
+    author='Andrés García Martínez',
+    author_email='ppnoptimizer@gmail.com',
+    packages=['entoolkit'],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent"])
